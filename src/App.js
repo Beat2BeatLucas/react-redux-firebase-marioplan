0e6b491748cf8cb5a5c0e373f0c017087c599ad2
Lucas Acuña <lucasnahuelnet@gmail.com>
2020-03-19 15:26:21 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <div className="App">
+        <h1>MarioPlan</h1>
+      </div>
+    </BrowserRouter>
+    
+  );
+}
+
+export default App;
