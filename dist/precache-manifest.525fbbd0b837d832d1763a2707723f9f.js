bf2d98416bb0ed721a0722cc87c34426c08bb142
Lucas Acuña <lucasnahuelnet@gmail.com>
2020-03-22 19:07:09 -0300

    Deploying to Firebase Hosting

---
+++ dist/precache-manifest.525fbbd0b837d832d1763a2707723f9f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "720093797b2e9c90747dad9ea0ad3167",
+    "url": "/index.html"
+  },
+  {
+    "revision": "18d068aa47524c746cce",
+    "url": "/static/css/main.e31c02a2.chunk.css"
+  },
+  {
+    "revision": "ce29ccba62923b289fa8",
+    "url": "/static/js/2.7b3b00c7.chunk.js"
+  },
+  {
+    "revision": "6b9f671524e4817f679e05b83b5beb67",
+    "url": "/static/js/2.7b3b00c7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "18d068aa47524c746cce",
+    "url": "/static/js/main.5d0eb5e6.chunk.js"
+  },
+  {
+    "revision": "5ae5cdff232dc0214069",
+    "url": "/static/js/runtime-main.a8803374.js"
+  }
+]);
